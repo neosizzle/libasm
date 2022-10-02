16d7dca15c0f7140c35045dfe99e088af9fa2283
neosizzle <junahnngd1115@gmail.com>
2022-10-02 16:25:27 +0800

    ADD ft_list_sort

---
+++ ft_list_sort_bonus.s
@@ -0,0 +1,70 @@
+section .text
+
+; rdi - begin_list
+; rsi - compare func
+; r10 - current node
+; rdx - temp variable to store data
+; rbx - next node after curr
+; r8 - begin_list copy
+; r9 - cmp func copy
+global ft_list_sort
+ft_list_sort:
+	; stack frame init
+	PUSH rbp ; push the current bottom of stack to memory
+	PUSH rdi ; save rdi to memory
+	PUSH rsi ; save rsi to memory
+	PUSH r10 ; save r10 to memory
+	PUSH rdx ; save rdx to memory
+	PUSH rbx ; save rbx to memory
+	PUSH r8 ; save r8 to memory
+	PUSH r9 ; save r9 to memory
+	MOV rbp, rsp ; move the bottom stack pointer to curr top stack (create new stack)
+
+	CMP rdi, 0; check if begin list is 0;
+	JE clean_ft_list_sort; if yes, skip to clean
+	CMP word [rdi], 0; check if val of begin list is 0;
+	JE clean_ft_list_sort; if yes, skip to clean
+
+	MOV r10, [rdi] ; curr = *begin_list
+
+	MOV r8, rdi; begin list copy
+	MOV r9, rsi; cmp func copy
+
+iter_loop:
+	CMP word [r10 + 8], 0 ; curr->next === 0
+	JE clean_ft_list_sort ; jump to clean
+	MOV rbx, [r10 + 8] ; load next node to rbx
+	MOV rdi, [r10] ; move curr->data to first arg
+	MOV rsi, [rbx] ; move next->data to 2nd arg
+	CALL r9 ; call r9
+	; CMP rax, 0; campare ret value with zero
+	; TEST rax, rax
+	JLE increm_loop ; jump to increm lop if ret <= 0
+	JG move_data_loop ; jump to move_data_loop if res > 0
+
+move_data_loop :
+	MOV rdx, [r10] ; temp = curr->data
+	MOV [r10], rsi ; curr->data = next->data
+	MOV [rbx], rdx ; curr->next->data = temp
+	MOV r10, [r8] ; curr = *begin_list
+	JMP iter_loop_end; jump to loop end
+
+increm_loop :
+	ADD r10, 8; increm r10 by 8 so get -> next
+	MOV r10, [r10] ; curr = curr->next
+	JMP iter_loop_end; jump to loop end
+
+iter_loop_end:
+	JMP iter_loop; jump to loop start
+
+clean_ft_list_sort:
+	; stack frame cleanup
+	POP r9 ; restore r9
+	POP r8 ; restore r8
+	POP rbx ; restore rbx
+	POP rdx ; restore rdx
+	POP r10 ; restore r10
+	POP rsi ; restore rsi
+	POP rdi ; restore rdi
+	POP rbp ; restore prev bottom stack to register slot
+	ret; return
--- test_srcs/bonus_main.c
+++ test_srcs/bonus_main.c
@@ -13,25 +13,57 @@ typedef struct		s_list
 extern	int	ft_atoi_base(char *str, char *base);
 extern void ft_list_push_front(t_list **begin_list, void *data);
 extern int 	ft_list_size(t_list *begin_list);
+extern int ft_list_sort(t_list **begin_list, int (*cmp)());
+// extern int ref_ft_list_sort(t_list **begin_list, int (*cmp)());
 
+static void	ref_ft_list_sort(t_list **begin_list, int (*cmp)())
+{
+	void	*temp;
+	t_list	*curr;
+
+	if (!begin_list || !*begin_list)
+		return ;
+	curr = *begin_list;
+	while (curr->next)
+	{
+		if (cmp(curr->data, curr->next->data) > 0)
+			{
+				temp = curr->data;
+				curr->data = curr->next->data;
+				curr->next->data = temp;
+				curr = *begin_list;
+			}
+		else
+			curr = curr->next;
+	}
+}
+
+
+static void print_list(t_list *head) {
+	while (head)
+	{
+		printf("%s\n", (char *)head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
 
 int	main(int argc, char **argv)
 {
-	t_list * list = malloc(sizeof (t_list));
-	list->data = strdup("second");
-	list->next = NULL;
+	// t_list * list = malloc(sizeof (t_list));
+	// list->data = strdup("second");
+	// list->next = NULL;
+	t_list *list = NULL;
 
 	t_list **head = &list;
-	// list->num = 696969;
-	// list->somechar = 'P';
-	// printf("first data b4 |%s| \n", (char *)(*head)->data);
 
-	ft_list_push_front(head, strdup("asdf"));
-	printf("first data |%s| \n", (char *)(*head)->data);
-	printf("2nd data |%s| \n", (char *)(*head)->next->data);
-	ft_list_push_front(head, strdup("asdf"));
-	ft_list_push_front(head, strdup("asdf"));
-	ft_list_push_front(head, strdup("asdf"));
-	printf("size %d\n", ft_list_size(*head));
+	ft_list_push_front(&list, strdup("first"));
+	ft_list_push_front(&list, strdup("sec"));
+	ft_list_push_front(&list, strdup("thirds"));
+	ft_list_push_front(&list, strdup("thirds"));
+	print_list(list);
+
+	ft_list_sort(&list, strcmp);
+	print_list(list);
 	return 0;
 }
